c05385f8927bc059895dcd819bc54892d05d3e1d
Kanchan <kanchansingh30nov@gmail.com>
2024-03-28 17:08:19 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'poppins': ['Poppins', 'Helvetica', 'Arial','sans-serif']
+      }
+    },
+
+  },
+  plugins: [],
+}
+
